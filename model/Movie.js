5418ae5e268b814fbd81cb9069c4ad7da71e854b
hkdemircan <hkdemircan06@gmail.com>
2019-08-31 00:44:36 +0300

    movie update,delete, search servisleri yazıldı.

--- model/Movie.js
+++ model/Movie.js
@@ -13,7 +13,7 @@ const MovieSchema = new Schema({
     country: String,
     year: Number,
     imdb_score: Number,
-    date: {
+    createdAt: {
         type: Date,
         default: Date.now
     }
--- routes/movie.js
+++ routes/movie.js
@@ -8,16 +8,62 @@ const Movie = require('../model/Movie');
 router.get('/', (req, res) =>{
   const promise = Movie.find({ });
   promise.then((data)=>{
+
     res.json(data);
   }).catch((err)=>{
     res.json(err);
   });
 });
 
+
+// imdb score'a gore top 10 listesi çekme
+router.get('/top10', (req, res) =>{
+  const promise = Movie.find({ }).limit(10).sort({imdb_score: -1});
+  promise.then((data)=>{
+
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
 // id'ye göre film çekme
-router.get('/:movie_id', (req, res)=>{
+router.get('/:movie_id', (req, res, next)=>{
   const promise = Movie.findById(req.params.movie_id);
   promise.then((data) => {
+    if(!data)
+      next({message: 'The movie was not found', code: 99});
+
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+// id'ye gore film güncelleme.
+// new: true -> günccellenmiş datayı döndürmek için.
+router.put('/:movie_id', (req, res, next)=>{
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true});
+
+  promise.then((data) => {
+    if(!data)
+      next({message: 'The movie was not found', code: 99});
+
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
+// id'ye göre film silme
+router.delete('/:movie_id', (req, res, next)=>{
+  const promise = Movie.findByIdAndDelete(req.params.movie_id);
+  promise.then((data) => {
+    if(!data)
+      next({message: 'The movie was not found', code: 99});
+
     res.json(data);
   }).catch((err)=>{
     res.json(err);
@@ -60,6 +106,22 @@ router.post('/', (req, res, next) => {
     res.json(err);
   });
 
+});
+
+// iki yıl arasındaki filmleri çekme
+// gte -> büyük veya eşit anlamında
+// lte -> küçük veya eşit anlamında
+router.get('/between/:start_year/:end_year', (req, res) =>{
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find({
+    year: { "$gte": start_year, "$lte": end_year}
+   });
+  promise.then((data)=>{
+
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
 });
 
 module.exports = router;
