b97d56b6525eb7cc4a1111c07a200455abe09a82
hkdemircan <hkdemircan06@gmail.com>
2019-08-31 19:29:08 +0300

    user kayıt yapıldı

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+//Models
+const User = require('../model/User.js');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+//create user
+router.post('/register', (req, res, next) => {
+  const {username, password} = req.body;
+
+  const user = new User({
+    username,
+    password
+  });
+
+  const promise = user.save();
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
 module.exports = router;
