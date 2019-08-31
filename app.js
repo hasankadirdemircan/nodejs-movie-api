fa603123e13bea4594965ec3eea29a3ff05d2292
hkdemircan <hkdemircan06@gmail.com>
2019-08-31 22:15:57 +0300

    jwt authenticate yapıldı.

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const db = require('./helper/db.js')(); //fonksiyonu calistiriyor.
 const config = require('./config');
 app.set('api_secret_key', config.api_secret_key);
 
+//Middleware jwt
+const verifyToken = require('./middleware/verify-token')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movie', movie);
 app.use('/api/director', director);
 
