763707a711787dc2d916fa7272be45ce60a6ff3a
hkdemircan <hkdemircan06@gmail.com>
2019-08-31 16:44:42 +0300

    movie schema için validasyon eklendi.

--- model/Movie.js
+++ model/Movie.js
@@ -2,17 +2,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+// value -> girilen deger.
+// path -> ilgili alan ismi.
 const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '{PATH} alanı zorunludur'],
+        maxlength: [15, '{PATH} alanı {VALUE}, {MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [1, '{PATH} alanı {VALUE}, {MINLENGTH} karakterden büyük olmalııdr.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
